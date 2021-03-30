36f0821644d4ac637bbbe3f9eb535df695957cef
Lucas Gomes <lucas.gomesgs0@gmail.com>
2021-03-30 19:31:09 -0300

    Created activity diagram and node class

---
+++ TP1/Classes/Node.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP1.Classes
+{
+    public class Node
+    {
+        public string Name { get; set; }
+        public string Source { get; set; }
+        public string Target { get; set; }
+        public float Prob { get; set; }
+        public NodeTypes NodeType { get; set; }
+
+        public Node()
+        {
+
+        }
+    }
+}
--- TP1/Program.cs
+++ TP1/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using TP1.Services;
 
 namespace TP1
 {
@@ -6,7 +8,16 @@ namespace TP1
     {
         static void Main(string[] args)
         {
+            var serviceCollection = new ServiceCollection();
+            ConfigureServices(serviceCollection);
+            var serviceProvider = serviceCollection.BuildServiceProvider();
+
             Console.WriteLine("Hello World!");
+        }
+
+        public static void ConfigureServices(IServiceCollection services)
+        {
+            services.AddScoped<NodeService>();
         }
     }
 }
