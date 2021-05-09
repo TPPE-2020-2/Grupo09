8c9c76cfc548f082afe236112ad58b2239a21654
Lucas Gomes <lucas.gomesgs0@gmail.com>
2021-05-09 16:48:35 -0300

    Substituir condicional por polimorfismo
    A operação de refatoração foi aplicada no método ToString na classe SequenceRoot. Foi criada cinco classes e todas elas com um método ToString para retornar as informações para o arquivo XML daquele tipo de elemento em específico. Quatro dessas classes criadas herdam da superclasse List e uma classe herda da classe Dictionary, onde sobrescrevem o método ToString e são implementadas de acordo com os comportamentos esperados por cada classe.

--- UML/Sequence/Message.cs
+++ UML/Sequence/Message.cs
@@ -20,5 +20,28 @@ namespace TPPE1.Sequence
         public Lifeline Target { get; set; }
 
         public MessageTypes Type { get; set; }
+
+        public override string ToString()
+        {
+            return $"\t\t<{ElementType} name=\"{Name}\" prob=\"{Prob}\" source=\"{Source.Name}\" target=\"{Target.Name}\"/>\n";
+        }
+    }
+
+    public class MessageList : List<Message>
+    {
+        public override string ToString()
+        {
+            if (Count == 0)
+                return null;
+
+            var content = "";
+
+            foreach (var message in this)
+            {
+                content += message;
+            }
+
+            return content;
+        }
     }
 }
--- UML/Sequence/Optional.cs
+++ UML/Sequence/Optional.cs
@@ -9,5 +9,31 @@ namespace TPPE1.Sequence
         public override ElementTypes ElementType => ElementTypes.Optional;
 
         public SequenceDiagram RepresentedBy { get; set; }
+
+        public override string ToString()
+        {
+            return $"\t\t<{ElementType} name=\"{Name}\" representedBy=\"{RepresentedBy.Name}\"/>\n";
+        }
+    }
+
+    public class OptionalList : List<Optional>
+    {
+        public override string ToString()
+        {
+            if (Count == 0)
+                return null;
+
+            var content = "";
+            content += $"\t<Fragments>\n";
+
+            foreach (var optional in this)
+            {
+                content += optional;
+            }
+
+            content += $"\t</Fragments>\n";
+
+            return content;
+        }
     }
 }
